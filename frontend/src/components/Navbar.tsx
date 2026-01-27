d4793f8d27e2727716bb31f8e824338e355a8538
Syeda Samra Ali <alisamra720@gmail.com>
2026-01-27 15:54:11 +0500

    installed react-icons and used in the navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,5 @@
+import { MdArrowOutward } from "react-icons/md";
+
 function Navbar() {
   return (
     <nav className="flex items-center justify-between fixed top-0 left-0 w-full z-50 bg-transparent text-white px-24 pt-10">
@@ -6,19 +8,28 @@ function Navbar() {
       <ul className="flex gap-10 text-gray-300">
         <li className="hover:text-white transition cursor-pointer border-t-2">
           <div className=" w-42.5">
-            <h3 className="font-semibold mb-2 pt-1">My Projects</h3>
+            <div className="flex justify-between">
+              <h3 className="font-bold text-xl mb-2 pt-1">My Projects</h3>
+              <MdArrowOutward className="my-auto" />
+            </div>
             <p>See all the nice projects I've done</p>
           </div>
         </li>
         <li className="hover:text-white transition cursor-pointer border-t-2">
           <div className="w-42.5">
-            <h3 className="font-semibold mb-2 pt-1">About Me</h3>
+            <div className="flex justify-between">
+              <h3 className="font-bold text-xl mb-2 pt-1">About Me</h3>
+              <MdArrowOutward className="my-auto" />
+            </div>
             <p>Learn about my self what I do</p>
           </div>
         </li>
         <li className="hover:text-white transition cursor-pointer border-t-2">
           <div className="w-42.5">
-            <h3 className="font-semibold mb-2 pt-1">Contact me</h3>
+            <div className="flex justify-between">
+              <h3 className="font-bold text-xl mb-2 pt-1">Contact me</h3>
+              <MdArrowOutward className="my-auto" />
+            </div>
             <p>alisamra720@gmail.com</p>
           </div>
         </li>
