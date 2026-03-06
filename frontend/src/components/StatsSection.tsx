235c014c29c2af165e5c1cc7eb6ab1a7f59236d5
Syeda Samra Ali <alisamra720@gmail.com>
2026-03-06 20:33:51 +0500

    StatsSection made responsive

--- frontend/src/components/StatsSection.tsx
+++ frontend/src/components/StatsSection.tsx
@@ -2,28 +2,33 @@ import { FaListCheck } from "react-icons/fa6";
 
 export default function StatsSection() {
   return (
-    <section className="text-main px-24 mb-10">
-      <div className="grid grid-cols-4">
-        <div className="border-y border-r p-14">
-          <FaListCheck className="text-6xl" />
-          <h1 className="text-5xl font-semibold my-5">2450</h1>
-          <p className="text-muted text-md">Projects Completed Done</p>
+    <section className="text-main px-6 md:px-12 xl:px-24 mb-10">
+      <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
+        
+        <div className="border-y md:border-r p-10 xl:p-14 flex flex-col items-center xl:items-start text-center xl:text-left">
+          <FaListCheck className="text-5xl md:text-6xl text-main" />
+          <h1 className="text-4xl md:text-5xl font-semibold my-5">2450</h1>
+          <p className="text-muted text-md uppercase tracking-wider">Projects Completed Done</p>
         </div>
-        <div className="border-y border-r p-14">
-          <FaListCheck className="text-6xl" />
-          <h1 className="text-5xl font-semibold my-5">1085</h1>
-          <p className="text-muted text-md">Satisfied Clients</p>
+
+        <div className="border-b md:border-y xl:border-r p-10 xl:p-14 flex flex-col items-center xl:items-start text-center xl:text-left">
+          <FaListCheck className="text-5xl md:text-6xl text-main" />
+          <h1 className="text-4xl md:text-5xl font-semibold my-5">1085</h1>
+          <p className="text-muted text-md uppercase tracking-wider">Satisfied Clients</p>
         </div>
-        <div className="border-y border-r p-14">
-          <FaListCheck className="text-6xl" />
-          <h1 className="text-5xl font-semibold my-5">07</h1>
-          <p className="text-muted text-md">My Team Members</p>
+
+        <div className="border-b md:border-y md:border-r p-10 xl:p-14 flex flex-col items-center xl:items-start text-center xl:text-left">
+          <FaListCheck className="text-5xl md:text-6xl text-main" />
+          <h1 className="text-4xl md:text-5xl font-semibold my-5">07</h1>
+          <p className="text-muted text-md uppercase tracking-wider">My Team Members</p>
         </div>
-        <div className="border-y p-14">
-          <FaListCheck className="text-6xl" />
-          <h1 className="text-5xl font-semibold my-5">2790</h1>
-          <p className="text-muted text-md">World Wide Customers</p>
+
+        <div className="border-b md:border-y p-8 xl:p-12 flex flex-col items-center xl:items-start text-center xl:text-left">
+          <FaListCheck className="text-5xl md:text-6xl text-main" />
+          <h1 className="text-4xl md:text-5xl font-semibold my-5">2790</h1>
+          <p className="text-muted text-md uppercase tracking-wider">World Wide Customers</p>
         </div>
+
       </div>
     </section>
   );
